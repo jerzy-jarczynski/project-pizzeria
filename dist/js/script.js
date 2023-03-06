13ed8b90adba56716613fce0ebdd9d5f47c2fc46
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-03-06 21:53:55 +0100

    Add JSON server connection and get products

--- dist/js/script.js
+++ dist/js/script.js
@@ -71,12 +71,15 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }, // CODE CHANGED
-    // CODE ADDED START
+    },
     cart: {
       defaultDeliveryFee: 20,
     },
-    // CODE ADDED END
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
   
   const templates = {
@@ -565,14 +568,30 @@
       console.log('thisApp.data:', thisApp.data);
 
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function() {
       const thisApp = this;
 
-      thisApp.data = dataSource; // zapytać Patryka jak jest przekazywana ta zmienna z pliku data.js
+      const url = settings.db.url + '/' + settings.db.products;
+
+      thisApp.data = {};
+
+      fetch(url)
+        .then(function(rawResponse) {
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse) {
+          console.log('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+
+          /* execute initMenu method */
+          thisApp.initMenu();
+        });
     },
 
     initCart: function() {
@@ -591,7 +610,6 @@
       console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
