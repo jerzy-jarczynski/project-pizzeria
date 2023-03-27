f4b68cd0b17f91a79f2c9c71a0f400561d7c41ad
jerzy-jarczynski <123597009+jerzy-jarczynski@users.noreply.github.com>
2023-03-27 09:42:39 +0200

    Update settings.db.url

--- src/js/settings.js
+++ src/js/settings.js
@@ -129,7 +129,7 @@ export const settings = {
     inputHoursName: 'hours',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     bookings: 'bookings',
